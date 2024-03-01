852a883a87a447b29eb8276d7d6faabbbb1319fb
Qluka <ryze634@gmail.com>
2024-03-01 20:29:27 +0400

    commit

---
+++ day8/homework.py
@@ -0,0 +1,9 @@
+required_weight=50
+required_height=170
+weight=int(input ("enter your weight"))
+height= int(input ("enter your height"))
+print(required_height > height)
+
+print(required_weight>weight)
+print(required_height>height)
+
